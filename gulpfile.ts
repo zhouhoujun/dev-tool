b23dcf1e1d36cf3c069e2cf30ca4f305be694d41
Houjun <freebirds8281@hotmail.com>
2016-10-23 00:59:51 +0800

    use node build, add source maps

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,22 +1,10 @@
 import * as gulp from 'gulp';
+import { Development } from './src/tools';
 
-const ts = require('gulp-typescript');
-const merge = require('merge2');
-const del = require('del');
-
-
-gulp.task('build', ['clean-lib'], () => {
-    let tsProject = ts.createProject('tsconfig.json');
-    let tsResult = gulp.src('src/**/*.ts')
-        .pipe(tsProject());
-
-    return merge([
-        // Merge the two output streams, so this task is finished when the IO of both operations are done. 
-        tsResult.dts.pipe(gulp.dest('lib')),
-        tsResult.js.pipe(gulp.dest('lib'))
-    ]);
-});
-
-gulp.task('clean-lib', () => {
-    return del('lib');
+Development.create(gulp, __dirname, {
+    tasks: {
+        src: 'src',
+        dist: 'lib',
+        loader: 'development-tool-node'
+    }
 });
